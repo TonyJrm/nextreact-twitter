f282d1edf88b2fe14055a5ec9855a59335e9e1ec
Tony JEROME <tony.jerome@include-tech.fr>
2024-08-09 17:19:05 +0200

    Exercice 1 - Part 3 done

--- pages/exercices/1.tsx
+++ pages/exercices/1.tsx
@@ -33,10 +33,60 @@ const TweetsScheme = z.object({
   ),
 });
 
+export type ClientConfig<T> = {
+  data?: unknown;
+  zodSchema?: z.ZodSchema<T>;
+  method?: 'DELETE' | 'GET' | 'OPTIONS' | 'PATCH' | 'POST' | 'PUT';
+  headers?: HeadersInit;
+  customConfig?: RequestInit;
+  signal?: AbortSignal;
+};
+
+export async function client<T>(
+  url: string,
+  {
+    data,
+    zodSchema,
+    method,
+    headers: customHeaders,
+    signal,
+    customConfig,
+  }: ClientConfig<T> = {}
+): Promise<T> {
+  const config: RequestInit = {
+    method: method ?? (data ? 'POST' : 'GET'),
+    body: data ? JSON.stringify(data) : null,
+    headers: {
+      'Content-Type': data ? 'application/json' : '',
+      Accept: 'application/json',
+      ...customHeaders,
+    },
+    signal,
+    ...customConfig,
+  };
+
+  return window.fetch(url, config).then(async (response) => {
+    if (response.status === 401) {
+      return Promise.reject(new Error("You're not authenticated"));
+    }
+
+    let result = null;
+    try {
+      result = response.status === 204 ? null : await response.json();
+    } catch (error: unknown) {
+      return Promise.reject(error);
+    }
+
+    if (response.ok) {
+      return zodSchema && result ? zodSchema.parse(result) : result;
+    } else {
+      return Promise.reject(result);
+    }
+  });
+}
+
 const getTweets = async (signal: AbortSignal) =>
-  fetch('/api/tweets', { signal })
-    .then((res) => res.json())
-    .then((json) => TweetsScheme.parse(json));
+  client('/api/tweets', { signal, zodSchema: TweetsScheme });
 
 export default function FetchAllTweets() {
   const [tweets, setTweets] = useState<TlTweets | null>(null);
@@ -46,9 +96,7 @@ export default function FetchAllTweets() {
 
     getTweets(controller.signal)
       .then((data) => {
-        const validData = TweetsScheme.parse(data);
-
-        setTweets(validData.tweets);
+        setTweets(data.tweets);
       })
       .catch((err) => {
         if (err.name === 'AbortError') return;
