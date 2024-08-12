060c47a4c3517ac42e1a0afa9eac2fe82813dc66
Tony JEROME <tony.jerome@include-tech.fr>
2024-08-12 16:57:01 +0200

    SSR Exercise - Part 2 done.

--- pages/exercices/tweets/[tweetId]/4.tsx
+++ pages/exercices/tweets/[tweetId]/4.tsx
@@ -1,17 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
 import type { IncomingMessage } from 'http';
 import { AddTweet } from '~/components/tweets/AddTweet';
 import { TweetWithLikes } from '~/components/tweets/TweetWithLikes';
 import TwitterLayout from '~/components/TwitterLayout';
 import { getTweet } from '~/db/tweets';
+import { client } from '~/lib/client/client';
 import { getOptionalUserIdInCookie } from '~/lib/client/getUserIdCookie';
-import type { TweetView } from '~/lib/scheme/tweets';
+import { TweetScheme, type TweetView } from '~/lib/scheme/tweets';
+import { tweetKeys } from '~/lib/tweets/query.tweet';
 
-export default function TweetId({ tweet }: { tweet: TweetView }) {
+const getTweetsFromAPI = async (tweetId: string) => {
+  return client(`/api/tweets/${tweetId}`, {
+    zodSchema: TweetScheme,
+  });
+};
+
+export default function TweetId({ tweet: defaultTweet }: { tweet: TweetView }) {
+  const { data } = useQuery({
+    queryKey: tweetKeys.getById(defaultTweet.id),
+    queryFn: () => getTweetsFromAPI(defaultTweet.id),
+    initialData: {
+      tweet: defaultTweet,
+    },
+  });
+
+  const tweet = data.tweet;
+
   return (
     <TwitterLayout>
-      {/* 🦁 Affiche le TweetWithLikes */}
       <TweetWithLikes tweet={tweet} parentTweetId={tweet.id} />
-      <AddTweet />
+      <AddTweet tweetId={tweet.id} />
       <h2 className="p-4 text-xl font-bold">Replies</h2>
       {tweet.replies?.map((reply) => (
         <TweetWithLikes key={reply.id} tweet={reply} parentTweetId={tweet.id} />
@@ -25,15 +43,12 @@ export const getServerSideProps = async (context: {
   req: IncomingMessage;
 }) => {
   const { tweetId } = context.params;
-  // 🦁 Récupère le userId avec la fonction getOptionalUserIdInCookie
   const userId = getOptionalUserIdInCookie(context.req);
 
-  // 🦁 Récupère le tweet avec la fonction getTweet
   const tweet = await getTweet(tweetId, userId);
 
   return {
     props: {
-      // ⚠️ Utilise le trick de JSON pour copier l'objet tweet
       tweet,
     },
   };
